87e7610fbf7e5db5e2069acc633e88a5deb418ca
Lokanathreddy1 <lokanathreddyhai@gmail.com>
2021-11-12 16:22:57 +0530

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -6,4 +6,8 @@ def test_url_shortener():
     a = 10
     b = 20
     result = sum_off(a, b)
+    import os
+    print(os.getenv("abcd"))
+    print(os.getenv("cd"))
     assert result == 30
+    assert os.getenv("abcd") == 1234
